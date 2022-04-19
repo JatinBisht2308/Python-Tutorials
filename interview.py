c62b384587b008a1eb1c3abc05d9f788e70cb7ff
Jatin_Bisht <82670244+JatinBisht2308@users.noreply.github.com>
2022-04-19 19:24:47 +0000

    :fire: added some sort of comment to clear the interview

---
+++ interview.py
@@ -0,0 +1,19 @@
+# what is python?
+# what is programming language
+# why programming language
+# whay python is python
+# what are the applications of python
+# where we can code
+# how to use repl effectively
+# print statement
+# variables in python
+# operator basic 
+# one question that sir has given me to solve and the students only know that python is only a snake
+
+
+
+
+
+
+# Start with a quote
+# Never get upsest or demotivated during learning something specially python or nay other programming languages because there is infinite sources but a single person cant remember all the things so your work is to learn the basics and a bit advanced of a language specially python and the skill that you have to built is the thinking and creative mindset because you can every thing either game development, app dev, web dev, machine learning and also the iot technology and all. So sit back and take a deep breath becuase this fun interactive and full of knowledge course will be the life changing course for somoe of the students over here, i will tell you a story about my self (ma ek din aisi kisi workshop ma chala gya tha college ki and that workshop is the life changing workshop of my life i dont know what inspires me but before 1 year i was literally 0 at coding and building logics and now im your instructor but yaa it takes a lot of effort one thing i think i shoukd say in the beginning that i cant only teach the basics but in programming you learn more by doing and dint affraid of error in the because error are your freinds that push you to get better then the past so whenever you face any error be happy and give your 100 and 10 % to solve that error simlarly reach out new things and happy while acheiving failure in it because failures are the root of success.)
