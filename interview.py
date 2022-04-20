233b182ab12bc63e3a9eb8ff66c54bbe5b680235
Jatin_Bisht <82670244+JatinBisht2308@users.noreply.github.com>
2022-04-20 05:06:31 +0000

    :fire: stuff changes

--- interview.py
+++ interview.py
@@ -57,7 +57,7 @@ print("Substaction of ",x, "and ",y,"is:",sub)
 print("Multiplication of ",x, "and ",y,"is:",multi)
 print("Division of ",x, "and ",y,"is:",div)
 
-
+print("learning done")
 
 # Start with a quote
 # Never get upsest or demotivated during learning something specially python or nay other programming languages because there is infinite sources but a single person cant remember all the things so your work is to learn the basics and a bit advanced of a language specially python and the skill that you have to built is the thinking and creative mindset because you can every thing either game development, app dev, web dev, machine learning and also the iot technology and all. So sit back and take a deep breath becuase this fun interactive and full of knowledge course will be the life changing course for somoe of the students over here, i will tell you a story about my self (ma ek din aisi kisi workshop ma chala gya tha college ki and that workshop is the life changing workshop of my life i dont know what inspires me but before 1 year i was literally 0 at coding and building logics and now im your instructor but yaa it takes a lot of effort one thing i think i shoukd say in the beginning that i cant only teach the basics but in programming you learn more by doing and dint affraid of error in the because error are your freinds that push you to get better then the past so whenever you face any error be happy and give your 100 and 10 % to solve that error simlarly reach out new things and happy while acheiving failure in it because failures are the root of success.)
